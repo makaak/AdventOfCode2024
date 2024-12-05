8f2c2ce91a4852b857b2301c2ee61decb1254480
makaak <rauno.raidma@gmail.com>
2024-12-05 20:58:11 +0200

    Day 4 and day 5 solutions.

--- day4/solution.java
+++ day4/solution.java
@@ -3,6 +3,7 @@ package day4;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class solution {
     public static void main(String[] args) throws Exception {
@@ -11,8 +12,144 @@ public class solution {
         BufferedReader exReader = new BufferedReader(new FileReader(example));
         BufferedReader inReader = new BufferedReader(new FileReader(input));
         String line;
-        while ((line = exReader.readLine()) != null) {
-
+        ArrayList<String> data = new ArrayList<>();
+        while ((line = inReader.readLine()) != null) {
+            data.add(line);
         }
+
+        char[][] chars = new char[data.size()][data.getFirst().length()];
+        for (int i = 0; i < data.size(); i++) {
+            chars[i] = data.get(i).toCharArray();
+        }
+        int ans = 0;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars[i].length; j++) {
+                if (chars[i][j] == 'X') {
+                    try {
+                        if (chars[i][j + 1] == 'M' && chars[i][j + 2] == 'A' && chars[i][j + 3] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i][j - 1] == 'M' && chars[i][j - 2] == 'A' && chars[i][j - 3] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i - 1][j] == 'M' && chars[i - 2][j] == 'A' && chars[i - 3][j] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i + 1][j] == 'M' && chars[i + 2][j] == 'A' && chars[i + 3][j] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i + 1][j + 1] == 'M' && chars[i + 2][j + 2] == 'A'
+                                && chars[i + 3][j + 3] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i - 1][j + 1] == 'M' && chars[i - 2][j + 2] == 'A'
+                                && chars[i - 3][j + 3] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i - 1][j - 1] == 'M' && chars[i - 2][j - 2] == 'A'
+                                && chars[i - 3][j - 3] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i + 1][j - 1] == 'M' && chars[i + 2][j - 2] == 'A'
+                                && chars[i + 3][j - 3] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+
+                }
+            }
+        }
+
+        inReader.close();
+        exReader.close();
+        System.out.printf("P1 answer: %d%n", ans);
+
+        exReader = new BufferedReader(new FileReader(example));
+        inReader = new BufferedReader(new FileReader(input));
+
+        data.clear();
+        while ((line = inReader.readLine()) != null) {
+            data.add(line);
+        }
+
+        chars = new char[data.size()][data.getFirst().length()];
+        for (int i = 0; i < data.size(); i++) {
+            chars[i] = data.get(i).toCharArray();
+        }
+        ans = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            for (int k = 0; k < chars.length; k++) {
+                if (chars[i][k] == 'A') {
+                    try {
+                        if (chars[i - 1][k - 1] == 'M' && chars[i + 1][k + 1] == 'S' && chars[i - 1][k + 1] == 'M'
+                                && chars[i + 1][k - 1] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i - 1][k - 1] == 'M' && chars[i + 1][k + 1] == 'S' && chars[i + 1][k - 1] == 'M'
+                                && chars[i - 1][k + 1] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i - 1][k - 1] == 'S' && chars[i + 1][k + 1] == 'M' && chars[i - 1][k + 1] == 'M'
+                                && chars[i + 1][k - 1] == 'S') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                    try {
+                        if (chars[i - 1][k - 1] == 'S' && chars[i + 1][k + 1] == 'M' && chars[i - 1][k + 1] == 'S'
+                                && chars[i + 1][k - 1] == 'M') {
+                            ans += 1;
+                        }
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+                }
+            }
+        }
+
+        inReader.close();
+        exReader.close();
+        System.out.printf("P2 answer: %d%n", ans);
+
     }
 }
--- day5/solution.java
+++ day5/solution.java
@@ -3,15 +3,89 @@ package day5;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class solution {
+
+    static boolean[][] rules = new boolean[100][100];
+
+    static void initRules(int a, int b) {
+        rules[a][b] = true;
+    }
+
     public static void main(String[] args) throws Exception {
         File example = new File("day5/example.txt");
         File input = new File("day5/input.txt");
         BufferedReader exReader = new BufferedReader(new FileReader(example));
         BufferedReader inReader = new BufferedReader(new FileReader(input));
+        File example_rules = new File("day5/example_rules.txt");
+        File rulesFile = new File("day5/rules.txt");
+        BufferedReader example_rulesReader = new BufferedReader(new FileReader(example_rules));
+        BufferedReader rulesReader = new BufferedReader(new FileReader(rulesFile));
         String line;
-        while((line = exReader.readLine()) != null){
-    	 }
+
+        while ((line = rulesReader.readLine()) != null) {
+            initRules(Integer.parseInt(line.split("\\|")[0]), Integer.parseInt(line.split("\\|")[1]));
+        }
+        rulesReader.close();
+        example_rulesReader.close();
+
+        String[] data;
+        ArrayList<Integer> pages;
+        boolean failed = false;
+        int ans = 0;
+        while ((line = inReader.readLine()) != null) {
+            data = line.split(",");
+            pages = new ArrayList<>();
+            for (int i = 0; i < data.length; i++) {
+                pages.add(Integer.parseInt(data[i]));
+            }
+
+            for (int j = pages.size() - 1; j > -1 && !failed; j--) {
+                for (int i = 0; i < j && !failed; i++) {
+                    if (rules[pages.get(j)][pages.get(i)]) {
+                        failed = true;
+                    }
+                }
+            }
+            if (!failed) {
+                ans += pages.get(pages.size() / 2);
+            }
+            failed = false;
+        }
+        inReader.close();
+        exReader.close();
+        System.out.printf("P1 answer: %d%n", ans);
+
+        exReader = new BufferedReader(new FileReader(example));
+        inReader = new BufferedReader(new FileReader(input));
+        ans = 0;
+
+        while ((line = inReader.readLine()) != null) {
+            data = line.split(",");
+            pages = new ArrayList<>();
+            for (int i = 0; i < data.length; i++) {
+                pages.add(Integer.parseInt(data[i]));
+            }
+
+            for (int j = pages.size() - 1; j > -1; j--) {
+                for (int i = 0; i < j; i++) {
+                    if (rules[pages.get(j)][pages.get(i)]) {
+                        failed = true;
+                        pages.add(i, pages.remove(j));
+                        j++;
+                        break;
+                    }
+                }
+            }
+            if (failed) {
+                ans += pages.get(pages.size() / 2);
+            }
+            failed = false;
+        }
+        inReader.close();
+        exReader.close();
+        System.out.printf("P2 answer: %d%n", ans);
+
     }
 }
