8a7c1b03a52113a0df9547b27627ebe8408617b2
Rauno Raidma <rauno.raidma@gmail.com>
2024-12-13 09:32:51 +0200

    Day 11 solution.

--- day11/solution.java
+++ day11/solution.java
@@ -1,9 +1,8 @@
-package day11;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class solution {
     public static void main(String[] args) throws Exception {
@@ -11,53 +10,62 @@ public class solution {
         File input = new File("day11/input.txt");
         BufferedReader exReader = new BufferedReader(new FileReader(example));
         BufferedReader inReader = new BufferedReader(new FileReader(input));
-        String line;
-        ArrayList<ArrayList<ArrayList<Long>>> data = new ArrayList<>();
-        long time;
-        
-        while ((line = exReader.readLine()) != null) {
-            for (int i = 0; i < line.split(" ").length; i++) {  
-                data.add(new ArrayList<>());
-                data.get(i).add(new ArrayList<>());
-                data.get(i).get(0).add(Long.parseLong(line.split(" ")[i]));
+        String line, left, right;
+        HashMap<String, Long> data = new HashMap<>();
+        HashMap<String, Long> processed = new HashMap<>();
+        while ((line = inReader.readLine()) != null) {
+            for (int i = 0; i < line.split(" ").length; i++) {
+                data.put(line.split(" ")[i], 1l);
             }
         }
-        long left, right, ans = 0;        
-        ArrayList<Long> arrayList;
-        for (ArrayList<ArrayList<Long>> e : data) {
-            
-        
-            for (int i = 0; i < 75; i++) {
-                e.add(new ArrayList<>());
-                arrayList = e.get(i);
-                time = System.currentTimeMillis();
-                for (int j = 0; j < arrayList.size(); j++) {
-                    if (arrayList.get(j) == 0) {
-                        // System.out.printf("Found 0 at position %d%n",j);
-                        e.get(i + 1).add(1l);
-                        continue;
+        long ans = 0, time;
+
+        for (int i = 0; i < 75; i++) {
+            time = System.currentTimeMillis();
+            for (String s : data.keySet()) {
+
+                if (s.equals("0")) {
+                    if (processed.containsKey("1")) {
+                        processed.put("1", processed.get("1") + data.get(s));
+                    } else {
+                        processed.put("1", data.get(s));
                     }
-                    if (arrayList.get(j).toString().length() % 2 == 0) {
-                        // System.out.printf("Found mod2 digits at position %d%n",j);
-                        left = arrayList.get(j) / (long) Math.pow(10, arrayList.get(j).toString().length() / 2);
-                        right = arrayList.get(j) - left * (long) Math.pow(10, arrayList.get(j).toString().length() / 2);
-                        e.get(i + 1).add(left);
-                        e.get(i + 1).add(right);
-                        
-                        // System.out.printf("Left: %s, Right: %s, new index: %d%n",left,right,j);
-                        continue;
-                    }
-                    // System.out.printf("Times 2024 at position %d%n", j);
-                    e.get(i + 1).add(arrayList.get(j) * 2024);
+
+                    continue;
                 }
-                // System.out.println(arrayList);
-                System.out.printf("Iteration: %d, %d elements, took %d ms%n", i, arrayList.size(),
-                        System.currentTimeMillis() - time);
-            }}
-        
-            for (ArrayList<ArrayList<Long>> e : data) {
-                ans += e.getLast().size();
+                if (s.length() % 2 == 0) {
+                    left = s.substring(0, s.length() / 2);
+                    right = Long.toString(Long.parseLong(s.substring(s.length() / 2)));
+
+                    if (processed.containsKey(left)) {
+                        processed.put(left, data.get(s) + processed.get(left));
+                    } else {
+                        processed.put(left, data.get(s));
+                    }
+
+                    if (processed.containsKey(right)) {
+                        processed.put(right, data.get(s) + processed.get(right));
+                    } else {
+                        processed.put(right, data.get(s));
+                    }
+
+                    continue;
+                }
+                if (processed.containsKey(Long.toString(Long.parseLong(s) * 2024))) {
+                    processed.put(Long.toString(Long.parseLong(s) * 2024),
+                            data.get(s) + processed.get(Long.toString(Long.parseLong(s) * 2024)));
+                } else {
+                    processed.put(Long.toString(Long.parseLong(s) * 2024), data.get(s));
+                }
             }
-            System.out.println(ans);
+            data = new HashMap<>(processed);
+            processed.clear();
+            System.out.printf("Iteration: %d, took %d ms%n", i, System.currentTimeMillis() - time);
+        }
+
+        for (String s : data.keySet()) {
+            ans += data.get(s);
+        }
+        System.out.println(ans);
     }
 }
